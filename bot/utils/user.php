adf5ad78122aed91396b394409f1aa699a4ce7d9
Erfan Mola <hi@erfanxd.ir>
2025-02-22 08:00:18 +0330

    Update User DB Info If Changed

--- bot/utils/user.php
+++ bot/utils/user.php
@@ -4,3 +4,17 @@ function getUser(int|string $user_id, array | string $fields = '*', mixed $conn)
 {
 	return DPXDBQuery('users', $fields, ['user_id' => $user_id], conn: $conn);
 }
+
+function createUser(array $fields, mixed $conn): array
+{
+	DPXDBInsert('users', $fields, $conn);
+	return getUser($fields['user_id'], '*', $conn);
+}
+
+function updateUser(int|string $user_id, array $fields, mixed $conn): void
+{
+	DPXDBUpdate('users', [
+		...$fields,
+		'updated_at' => date("Y-m-d H:i:s"),
+	], ['user_id' => $user_id], conn: $conn);
+}
